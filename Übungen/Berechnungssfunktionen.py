64cca5b96f5b4a9e6b1e096910635fc6fb7d29bf
n0rs <n0rs9485@googlemail.com>
2025-04-15 21:10:11 +0200

    Jo

--- Übungen/Berechnungssfunktionen.py
+++ Übungen/Berechnungssfunktionen.py
@@ -26,17 +26,17 @@ def unteres_quartil_wert(quartilwert, klassendichte, klasse, klassen_spalte):
     index = klassen_spalte[klassen_spalte == klasse].index
     return quartilwert / klassendichte.loc[index].values[0]
 
-def mittleres_quartil_wert(kumulierte_haeufigkeit, klassendichte, klasse, quartilwert=0.5):
-    kumulierte_haeufigkeit = pd.to_numeric(kumulierte_haeufigkeit, errors='coerce')
+#def mittleres_quartil_wert(kumulierte_haeufigkeit, klassendichte, klasse, quartilwert=0.5):
+#    kumulierte_haeufigkeit = pd.to_numeric(kumulierte_haeufigkeit, errors='coerce')
+#
+ #   untere_grenze = float(klasse.split("-<")[0])
 
-    untere_grenze = float(klasse.split("-<")[0])
-
-    vorherige_kumulierte_haeufigkeit = kumulierte_haeufigkeit[kumulierte_haeufigkeit < quartilwert].values[-1]
+ #   vorherige_kumulierte_haeufigkeit = kumulierte_haeufigkeit[kumulierte_haeufigkeit < quartilwert].values[-1]
 
 
-    dichte = klassendichte.loc[klassendichte.index[klassendichte.index == klasse]].values[0]
+  #  dichte = klassendichte.loc[klassendichte.index[klassendichte.index == klasse]].values[0]
 
-    return untere_grenze + (quartilwert - vorherige_kumulierte_haeufigkeit) / dichte
+   # return untere_grenze + (quartilwert - vorherige_kumulierte_haeufigkeit) / dichte
 
 if __name__ == "__main__":
     main()
